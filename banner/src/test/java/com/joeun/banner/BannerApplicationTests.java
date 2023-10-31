0327493c2859037748ec5b32f3f73d8c1c95b1ed
wwwalohacampus <h850415@naver.com>
2023-11-01 07:21:14 +0900

    🐱‍🏍banner

---
+++ banner/src/test/java/com/joeun/banner/BannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joeun.banner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
