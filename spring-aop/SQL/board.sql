219a2ca1d3991815e789c282bc60e84466bc7f5b
wwwalohacampus <h850415@naver.com>
2023-11-15 12:55:47 +0900

    🤹‍♀️ spring aop - SQL 추가

---
+++ spring-aop/SQL/board.sql
@@ -0,0 +1,11 @@
+CREATE TABLE `board` (
+  `board_no` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(100) NOT NULL,
+  `writer` varchar(100) NOT NULL,
+  `content` text,
+  `reg_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `upd_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `views` int NOT NULL DEFAULT '0',
+  PRIMARY KEY (`board_no`)
+) COMMENT='게시판';
